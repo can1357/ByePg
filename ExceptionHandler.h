475cb768b5515abd4949f7097b6674e05f0a8a18
Can Bölük <can.boluk89@gmail.com>
2019-10-19 20:39:17 +0200

    Reworked EH logic

--- ExceptionHandler.h
+++ ExceptionHandler.h
@@ -8,13 +8,13 @@ namespace ExceptionHandler
 {
 	// High-level exception handler callback, must be set only once
 	//
-	using FnRoutine = LONG( CONTEXT* ContextRecord, EXCEPTION_RECORD* ExceptionRecord );
-	static volatile FnRoutine* Routine = nullptr;
+	using FnExceptionCallback = LONG( CONTEXT* ContextRecord, EXCEPTION_RECORD* ExceptionRecord );
+	static volatile FnExceptionCallback* HlCallback = nullptr;
 
 	// IRQL:	HIGH_LEVEL
 	// IF:		0
 	//
-	static void HandleBugCheck()
+	static void HandleBugCheck( FnExceptionCallback* Cb )
 	{
 		// Get state at KeBugCheck(Ex) call
 		CONTEXT* BugCheckCtx = GetProcessorContext();
@@ -37,18 +37,16 @@ namespace ExceptionHandler
 			*( ULONG64* ) ( BugCheckCtx->Rsp + 0x28 )
 		};
 
-		// Spinlock if routine became disabled
-		while ( !Routine );
-
 		// Try parsing parameters
 		CONTEXT* ContextRecord = nullptr;
 		EXCEPTION_RECORD ExceptionRecord;
 		if ( BugCheck::Parse( &ContextRecord, &ExceptionRecord, BugCheckCtx ) )
 		{
 			// Try handling exception
-			if ( Routine( ContextRecord, &ExceptionRecord ) == EXCEPTION_CONTINUE_EXECUTION )
+			if ( Cb( ContextRecord, &ExceptionRecord ) == EXCEPTION_CONTINUE_EXECUTION )
 			{
 				// Revert IRQL to match interrupted routine
+				_disable();
 				__writecr8( BugCheckState->Cr8 );
 
 				// Restore context (These flags make the control flow a little simpler :wink:)
@@ -59,100 +57,55 @@ namespace ExceptionHandler
 		}
 
 		// Failed to handle, show blue screen
-		WaitForDebugger();
-		Routine = nullptr;
+		HlCallback = nullptr;
+		ProcessorIpiFrozen() = 0;
+		*KiFreezeExecutionLock = false;
 		return KeBugCheckEx( BugCheckCode, BugCheckArgs[ 0 ], BugCheckArgs[ 1 ], BugCheckArgs[ 2 ], BugCheckArgs[ 3 ] );
 	}
 
-	// IRQL:	HIGH_LEVEL
-	// IF:		0
-	//
 	static void OnFreezeNotification()
 	{
-		if ( !Routine ) return;
+		FnExceptionCallback* Cb = HlCallback;
+		if ( !Cb ) return;
 
-		// Decrement KiHardwareTrigger
+		// Clear KiBugCheckActive
+		KiBugCheckActive->Value = 0;
+
+		// Decrement counter
 		InterlockedDecrement( KiHardwareTrigger );
 
-		// Reset PRCB members manipulated to hijack control flow
-		ProcessorNestingLevel()--;
-		ProcessorIpiFrozen() = 0;
+		// Reset IpiFrozen
+		ProcessorIpiFrozen() = 5;
 
 		// Handle BugCheck
-		return HandleBugCheck();
+		return HandleBugCheck( Cb );
 	}
 
-	// IRQL:	>= DISPATCH_LEVEL
-	// IF:		U
-	//
 	static void OnBugCheckNotification()
 	{
-		if ( !Routine ) return;
+		FnExceptionCallback* Cb = HlCallback;
+		if ( !Cb ) return;
 
-		// Disable interrupts and raise IRQL to HIGH_LEVEL to match
-		// freeze notification environment.
-		//
-		_disable();
-		__writecr8( HIGH_LEVEL );
+		// Clear KiBugCheckActive
+		KiBugCheckActive->Value = 0;
 
-		// Decrement KiHardwareTrigger
+		// Decrement counter
 		InterlockedDecrement( KiHardwareTrigger );
 
-		while ( true )
-		{
-			// Until we hijack all bugcheck control flows:
-			//
-			while ( *KiHardwareTrigger )
-			{
-				// Iterate each processor:
-				for ( int i = 0; i < KeNumberProcessors; i++ )
-				{
-					KPRCB* Prcb = KeQueryPrcbAddress( i );
-
-					// Get context
-					if ( CONTEXT* Ctx = GetProcessorContext( Prcb ) )
-					{
-						// If processor invoked KeBugCheckEx
-						if ( Ctx->Rip == ( ULONG64 ) KeBugCheckPtr || Ctx->Rip == ( ULONG64 ) KeBugCheckExPtr )
-						{
-							// Invalidate Rip so we skip this check next time
-							Ctx->Rip++;
-
-							// At OnKeBugCheck2, not in freeze spinlock
-							if ( KiBugCheckActive->OwnerProcessorIndex == GetProcessorIndex( Prcb ) )
-								continue;
-
-							// Hijack control flow of the spinlock to end up at HalNotifyProcessorFreeze -> ::OnFreezeNotification
-							ProcessorNestingLevel( Prcb )++;
-							ProcessorIpiFrozen( Prcb ) = 5;
-						}
-					}
-				}
-			}
-
-			// Release KiBugCheckActive lock
-			// Note: Yes this is not 100% thread safe, but it can't get any better.
-			//
-			// Clear KiBugCheckActive spinlock
-			LONG PrevLockValue = InterlockedExchange( &KiBugCheckActive->Value, 0 );
-			_mm_mfence();
-
-			// If enter count was incremented again somehow
-			if ( *KiHardwareTrigger )
-			{
-				// And this somehow happened without another processor acquiring KiBugCheckActive
-				if ( InterlockedCompareExchange( &KiBugCheckActive->Value, PrevLockValue, 0 ) != 0 )
-				{
-					// Acquire lock again and continue loop
-					continue;
-				}
-			}
-
-			// Finally quit the loop...
-			break;
-		}
-
 		// Handle BugCheck
-		return HandleBugCheck();
+		return HandleBugCheck( Cb );
+	}
+
+	static void Initialize()
+	{
+		// Set KiFreezeExecutionLock
+		*KiFreezeExecutionLock = true;
+
+		// Set IpiFrozen = 5 for all KPRCB
+		KeIpiGenericCall( [ ] ( ULONG64 x )
+		{
+			ProcessorIpiFrozen() = 5;
+			return 0ull;
+		}, 0 );
 	}
 };
