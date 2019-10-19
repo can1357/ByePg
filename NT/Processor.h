a1f6b5b0870e1e388c3512b95c8d807936e26ff7
Can Bölük <can.boluk89@gmail.com>
2019-10-19 11:01:51 +0200

    Added NT Processor block internals

---
+++ NT/Processor.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <ntifs.h>
+
+// Define processor related structures
+//
+typedef struct _KPRCB KPRCB;
+
+typedef struct _KDESCRIPTOR
+{
+	USHORT Pad[ 3 ];
+	USHORT Limit;
+	void* Base;
+} KDESCRIPTOR;
+
+typedef struct _KSPECIAL_REGISTERS
+{
+	ULONG64 Cr0;
+	ULONG64 Cr2;
+	ULONG64 Cr3;
+	ULONG64 Cr4;
+	ULONG64 KernelDr0;
+	ULONG64 KernelDr1;
+	ULONG64 KernelDr2;
+	ULONG64 KernelDr3;
+	ULONG64 KernelDr6;
+	ULONG64 KernelDr7;
+	KDESCRIPTOR Gdtr;
+	KDESCRIPTOR Idtr;
+	USHORT Tr;
+	USHORT Ldtr;
+	ULONG MxCsr;
+	ULONG64 DebugControl;
+	ULONG64 LastBranchToRip;
+	ULONG64 LastBranchFromRip;
+	ULONG64 LastExceptionToRip;
+	ULONG64 LastExceptionFromRip;
+	ULONG64 Cr8;
+	ULONG64 MsrGsBase;
+	ULONG64 MsrGsSwap;
+	ULONG64 MsrStar;
+	ULONG64 MsrLStar;
+	ULONG64 MsrCStar;
+	ULONG64 MsrSyscallMask;
+} KSPECIAL_REGISTERS;
+
+// Import KeQueryPrcbAddress
+//
+extern "C" __declspec( dllimport ) KPRCB* KeQueryPrcbAddress( ULONG Number );
